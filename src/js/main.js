3c02247823b6d886c0d878797f9d118155aee934
Sergio Medina <sergiomedina.me@gmail.com>
2023-03-12 11:55:32 +0100

    Agregado de funcionalidad dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -29,3 +29,28 @@ function toggle_search() {
 
 open_search.addEventListener("click", toggle_search);
 closed_search.addEventListener("click", toggle_search);
+
+
+// Dark mode
+const button_dark = document.querySelector("#dark-mode");
+
+// Detectamos si el usuario tiene activo el tema dark en su ordenador en la primera visita a la web
+if(!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+}
+
+// Detectamos el tema que elige el usuario
+if(localStorage.getItem('theme') == 'dark') {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark');
+}
+
+button_dark.addEventListener('click', function() {
+    let current_theme = localStorage.getItem('theme') == "dark" ? 'light' : 'dark';
+
+    document.documentElement.classList.toggle('dark');
+
+    localStorage.setItem('theme', current_theme);
+});
